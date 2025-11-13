02c46e8bd2593a9f16ac598d66ca7935d5d45752
DarkFenix <96185451+pedromarcs@users.noreply.github.com>
2025-11-13 22:55:30 +0000

    teste faculdade

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ export default function Header() {
     <>
       <HeaderContainer>
         <HeaderContent>
-          <Logo href="/">ADS Animes</Logo>
+          <Logo href="/">animes-brasil-nextjs</Logo>
 
           <Nav>
             <NavLink href="/">Home</NavLink>
